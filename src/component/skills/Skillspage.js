11ceb9b30d46646e2c169115b90620b58d301abb
Sakhumzi kwaza <sakhumzikwaza220@gmail.com>
2024-02-12 12:03:51 +0200

    Skills added

--- src/component/skills/Skillspage.js
+++ src/component/skills/Skillspage.js
@@ -1,139 +1,163 @@
-import React from 'react';
-function Skillspage(){
-    return(
-<div className="page">
-  
-    <div className="my_skills">
-        <i className="fa- fa-angle-left"></i>
-        <ul className="myskills_list">
-  
-          <li className="block_skill">
-            <h2>HTML</h2>
-            <div className="skill">
-              <span><i className="fa iconfonts fa-html5 fa-fade"></i></span>
+import React from "react";
+import "./Skillspage.css";
+
+function Skillspage() {
+  return (
+    <div className="skill_page">
+      <h2>Skills</h2>
+      <div className="container">
+        <div className="front_back">
+          <div className="front">
+            <div>
+              <h3>Front</h3>
             </div>
-            <div className="rating">
-              <span><i className="fas fa-star"></i></span>
-              <span><i className="fas fa-star"></i></span>
-              <span><i className="fas fa-star"></i></span>
-              <span><i className="fas fa-star"></i></span>
-              <span><i className="fas fa-star-half-stroke"></i></span>
-            </div>
-          </li>
-          
-          
-          <li className="block_skill">
-            <h2>CSS</h2>
-            <div className="skill">
-              <span><i className="fa iconfonts fa-css3 fa-fade"></i></span>
-            </div>
-            <div className="rating">
-              <span><i className="fas fa-star"></i></span>
-              <span><i className="fas fa-star"></i></span>
-              <span><i className="fas fa-star"></i></span>
-              <span><i className="fas fa-star"></i></span>
-              <span><i className="fas fa-star-half-stroke"></i></span>
-            </div>
-          </li>
-          <li className="block_skill">
-            <h2>JAVASCRIPT</h2>
-            <div className="skill">
-                <span><i className="fa iconfonts fa-js"></i></span>
-            </div>
-            <div className="rating">
-              <span><i className="fas fa-star"></i></span>
-              <span><i className="fas fa-star"></i></span>
-              <span><i className="fas fa-star"></i></span>
-              <span><i className="fas fa-star-half-stroke"></i></span>
-              <span><i className="fa-regular fa-star"></i></span>
-            </div>
-          </li>
-          <li className="block_skill">
-            <h2>NODEJS</h2>
-            <div className="skill">
-              <span><i className="fa iconfonts fa-node-js fa-fade"></i></span>
-            </div>
-            <div className="rating">
-              <span><i className="fas fa-star"></i></span>
-              <span><i className="fas fa-star"></i></span>
-              <span><i className="fas fa-star"></i></span>
-              <span><i className="fas fa-star-half-stroke"></i></span>
-              <span><i className="fa-regular fa-star"></i></span>
-            </div>
-          </li>
-          <li className="block_skill">
-            <h2>REACT NATIVE</h2>
-            <div className="skill">
-                <span><i className="fa iconfonts fa-react fa-fade"></i></span>
-            </div>
-            <div className="rating">
-              <span><i className="fas fa-star"></i></span>
-              <span><i className="fas fa-star"></i></span>
-              <span><i className="fas fa-star"></i></span>
-              <span><i className="fas fa-star-half-stroke"></i></span>
-              <span><i className="fa-regular fa-star"></i></span>
-            </div>
-          </li>
-        
-          <li className="block_skill">
-            <h2>JAVA</h2>
-            <div className="skill">
-              <span><i className="fa iconfonts fa-java fa-fade"></i></span>
-            </div>
-            <div className="rating">
-              <span><i className="fas fa-star"></i></span>
-              <span><i className="fas fa-star"></i></span>
-              <span><i className="fas fa-star"></i></span>
-              <span><i className="fas fa-star"></i></span>
-              <span><i className="fa-regular fa-star"></i></span>
-            </div>
-          </li>
-          <li className="block_skill">
-            <h2>PYTHON</h2>
-            <div className="skill">
-              <span><i className="fa iconfonts fa-python fa-fade"></i></span>
-            </div>
-            <div className="rating">
-              <span><i className="fas fa-star"></i></span>
-              <span><i className="fas fa-star"></i></span>
-              <span><i className="fas fa-star"></i></span>
-              <span><i className="fas fa-star"></i></span>
-              <span><i className="fa-regular fa-star"></i></span>
-            </div>
-          </li>
-          <li className="block_skill">
-            <h2>C++</h2>
-            <div className="skill">
-              <span><i className="fa iconfonts fa-plus fa-fade"></i></span>
-            </div>
-            <div className="rating">
-              <span><i className="fas fa-star"></i></span>
-              <span><i className="fas fa-star"></i></span>
-              <span><i className="fas fa-star"></i></span>
-              <span><i className="fa-regular fa-star"></i></span>
-              <span><i className="fa-regular fa-star"></i></span>
-            </div>
-          </li>
-        
-          <li className="block_skill">
-            <h2>MYSQL</h2>
-            <div className="skill">
-              <span><i className="fa iconfonts fa-database fa-fade"></i></span>
-            </div>
-            <div className="rating">
-              <span><i className="fas fa-star"></i></span>
-              <span><i className="fas fa-star"></i></span>
-              <span><i className="fas fa-star"></i></span>
-              <span><i className="fas fa-star"></i></span>
-              <span><i className="fa-regular fa-star"></i></span>
-            </div>
-          </li>
-  
-         
-        </ul>
-        <i id="right" className="fa fa-angle-right"></i>
+          </div>
+
+          <div className="back">
+            <h3>Back</h3>
+          </div>
+        </div>
       </div>
-</div>
-    );
+    </div>
+  );
 }
 export default Skillspage;
+
+
+
+// function Skillspage(){
+//     return(
+// <div className="page">
+
+//     <div className="my_skills">
+//         <i className="fa- fa-angle-left"></i>
+//         <ul className="myskills_list">
+
+//           <li className="block_skill">
+//             <h2>HTML</h2>
+//             <div className="skill">
+//               <span><i className="fa iconfonts fa-html5 fa-fade"></i></span>
+//             </div>
+//             <div className="rating">
+//               <span><i className="fas fa-star"></i></span>
+//               <span><i className="fas fa-star"></i></span>
+//               <span><i className="fas fa-star"></i></span>
+//               <span><i className="fas fa-star"></i></span>
+//               <span><i className="fas fa-star-half-stroke"></i></span>
+//             </div>
+//           </li>
+
+//           <li className="block_skill">
+//             <h2>CSS</h2>
+//             <div className="skill">
+//               <span><i className="fa iconfonts fa-css3 fa-fade"></i></span>
+//             </div>
+//             <div className="rating">
+//               <span><i className="fas fa-star"></i></span>
+//               <span><i className="fas fa-star"></i></span>
+//               <span><i className="fas fa-star"></i></span>
+//               <span><i className="fas fa-star"></i></span>
+//               <span><i className="fas fa-star-half-stroke"></i></span>
+//             </div>
+//           </li>
+//           <li className="block_skill">
+//             <h2>JAVASCRIPT</h2>
+//             <div className="skill">
+//                 <span><i className="fa iconfonts fa-js"></i></span>
+//             </div>
+//             <div className="rating">
+//               <span><i className="fas fa-star"></i></span>
+//               <span><i className="fas fa-star"></i></span>
+//               <span><i className="fas fa-star"></i></span>
+//               <span><i className="fas fa-star-half-stroke"></i></span>
+//               <span><i className="fa-regular fa-star"></i></span>
+//             </div>
+//           </li>
+//           <li className="block_skill">
+//             <h2>NODEJS</h2>
+//             <div className="skill">
+//               <span><i className="fa iconfonts fa-node-js fa-fade"></i></span>
+//             </div>
+//             <div className="rating">
+//               <span><i className="fas fa-star"></i></span>
+//               <span><i className="fas fa-star"></i></span>
+//               <span><i className="fas fa-star"></i></span>
+//               <span><i className="fas fa-star-half-stroke"></i></span>
+//               <span><i className="fa-regular fa-star"></i></span>
+//             </div>
+//           </li>
+//           <li className="block_skill">
+//             <h2>REACT NATIVE</h2>
+//             <div className="skill">
+//                 <span><i className="fa iconfonts fa-react fa-fade"></i></span>
+//             </div>
+//             <div className="rating">
+//               <span><i className="fas fa-star"></i></span>
+//               <span><i className="fas fa-star"></i></span>
+//               <span><i className="fas fa-star"></i></span>
+//               <span><i className="fas fa-star-half-stroke"></i></span>
+//               <span><i className="fa-regular fa-star"></i></span>
+//             </div>
+//           </li>
+
+//           <li className="block_skill">
+//             <h2>JAVA</h2>
+//             <div className="skill">
+//               <span><i className="fa iconfonts fa-java fa-fade"></i></span>
+//             </div>
+//             <div className="rating">
+//               <span><i className="fas fa-star"></i></span>
+//               <span><i className="fas fa-star"></i></span>
+//               <span><i className="fas fa-star"></i></span>
+//               <span><i className="fas fa-star"></i></span>
+//               <span><i className="fa-regular fa-star"></i></span>
+//             </div>
+//           </li>
+//           <li className="block_skill">
+//             <h2>PYTHON</h2>
+//             <div className="skill">
+//               <span><i className="fa iconfonts fa-python fa-fade"></i></span>
+//             </div>
+//             <div className="rating">
+//               <span><i className="fas fa-star"></i></span>
+//               <span><i className="fas fa-star"></i></span>
+//               <span><i className="fas fa-star"></i></span>
+//               <span><i className="fas fa-star"></i></span>
+//               <span><i className="fa-regular fa-star"></i></span>
+//             </div>
+//           </li>
+//           <li className="block_skill">
+//             <h2>C++</h2>
+//             <div className="skill">
+//               <span><i className="fa iconfonts fa-plus fa-fade"></i></span>
+//             </div>
+//             <div className="rating">
+//               <span><i className="fas fa-star"></i></span>
+//               <span><i className="fas fa-star"></i></span>
+//               <span><i className="fas fa-star"></i></span>
+//               <span><i className="fa-regular fa-star"></i></span>
+//               <span><i className="fa-regular fa-star"></i></span>
+//             </div>
+//           </li>
+
+//           <li className="block_skill">
+//             <h2>MYSQL</h2>
+//             <div className="skill">
+//               <span><i className="fa iconfonts fa-database fa-fade"></i></span>
+//             </div>
+//             <div className="rating">
+//               <span><i className="fas fa-star"></i></span>
+//               <span><i className="fas fa-star"></i></span>
+//               <span><i className="fas fa-star"></i></span>
+//               <span><i className="fas fa-star"></i></span>
+//               <span><i className="fa-regular fa-star"></i></span>
+//             </div>
+//           </li>
+
+//         </ul>
+//         <i id="right" className="fa fa-angle-right"></i>
+//       </div>
+// </div>
+//     );
+// }
+// export default Skillspage;
