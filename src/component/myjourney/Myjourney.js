d3fb7529cac70959637f2c2512bca92fd3d12734
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2024-02-26 03:47:21 +0200

    update height and width image

---
+++ src/component/myjourney/Myjourney.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import "./Myjourney.css";
+
+const Myjourney = () =>{
+    
+    return(
+       <section>
+        <div className="myjourney_page">
+            <div className="container">
+              <h3>JOURNEY</h3>
+
+              <div className="myjourney">
+                <div className="journey_containers">
+
+                </div>
+
+                <div className="journey_description">
+
+                </div>
+            </div>
+            </div>
+
+            
+        </div>
+       </section>
+    );
+}
+export default Myjourney;
