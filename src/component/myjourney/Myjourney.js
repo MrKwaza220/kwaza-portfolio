78036de73be2e9e07dd1f8fe3876556d125bffd5
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2024-02-27 01:24:42 +0200

    Journey added

--- src/component/myjourney/Myjourney.js
+++ src/component/myjourney/Myjourney.js
@@ -12,19 +12,37 @@ const Myjourney = () => {
             <div className="journey_containers">
               <div className="journey_container_1">
                 <div className="container_one">
-                    
+                  <h4>High School</h4>
+                  <p>Name of School: Ntsonkotha Senior</p>
+                  <p>Secondary School.</p>
+                  <p>Grade Passed: Grade 12</p>
+                  <p>Year Started: 2015</p>
+                  <p>Year ended: 2017</p>
                 </div>
                 <div className="container_two">
-
+                  <h4>Tertiary</h4>
+                  <p>Institution: University of Fort Hare</p>
+                  <p>Degree Level: Bachelor of Science</p>
+                  <p>Course: Computer Science</p>
+                  <p>Year Started: 2018</p>
+                  <p>Year ended: 2021</p>
                 </div>
               </div>
 
               <div className="journey_containers_2">
                 <div className="container_three">
-
+                  <h4>Capaciti Tect | UVU AFRICA</h4>
+                  <p>Job Title: Software Development</p>
+                  <p>Learnership.</p>
+                  <p>Duration: 6 Months</p>
+                  <p>Year Started: July 2023 - Present</p>
                 </div>
                 <div className="container_four">
-
+                  <h4>Younglings Africa</h4>
+                  <p>Job Title: Software Development</p>
+                  <p>Learnership.</p>
+                  <p>Duration: 12 Months</p>
+                  <p>Year Started: January 2023 - Present</p>
                 </div>
               </div>
             </div>
