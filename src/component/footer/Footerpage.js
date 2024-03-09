c44caadd0070b2aad479e62dfe0b178d1e7e2698
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2024-03-09 19:45:08 +0200

    added contact icons and links

--- src/component/footer/Footerpage.js
+++ src/component/footer/Footerpage.js
@@ -1,13 +1,25 @@
 import React from "react";
-import './Footerpage.css';
+import "./Footerpage.css";
 
-function Footerpage(){
-    return(
+function Footerpage() {
+  return (
     <footer>
       <div className="container">
-      <p>©Kwaza 2024. All rights reserved.</p>
+        <p>
+          <a href="https://github.com/MrKwaza220?tab=repositories">
+            <i className="fa fa-github animate"></i>
+          </a>
+          <a href="https://www.linkedin.com/in/sakhumzi-kwaza-2690351b9/">
+                <i className="fa fa-linkedin animate"></i>
+          </a>
+        </p>
+        <p>0730974288</p>
+        <p>sakhumzikwaza220@.gmail.com</p>
+        {""}
+        <br></br>
+        <p>©Kwaza 2024. All rights reserved.</p>
       </div>
     </footer>
-    );
+  );
 }
 export default Footerpage;
