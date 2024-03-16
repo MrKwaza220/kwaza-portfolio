b777243e7987f1596792e61adb1c87c51f386ab1
Sakhumzi Kwaza <sakhumzikwaza220@gmail.com>
2024-03-16 12:25:08 +0200

    added updated services

--- src/component/about/Aboutpage.js
+++ src/component/about/Aboutpage.js
@@ -1,83 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Aboutpage.css";
 
 const Aboutpage = () => {
-  // let sections = document.querySelectorAll("section");
+  // Define state to manage active tab
+  const [activeTab, setActiveTab] = useState('education');
 
-  // window.onscroll = () => {
-  //   sections.forEach((sec) => {
-  //     let top = window.scrollY;
-  //     let offset = sec.offsetTop - 150;
-  //     let height = sec.offsetHeight;
-
-  //     if (top >= offset && top < offset + height) {
-  //       sec.classList.add("show_animate");
-  //     } else {
-  //       sec.classList.add("show_animate");
-  //     }
-  //   });
-  // };
+  // Function to handle tab click
+  const handleTabClick = (tabName) => {
+    setActiveTab(tabName);
+  };
 
   return (
     <section className="sec_2 show_animate" id="about">
-    <div className="page animate">
-      <div className="container animate">
-        <div className="about_myself animate">
-          <div className="about_image animate">
-            <img src="sakhumzi.png " alt="" className="animate" />
-          </div>
-          
-          <div className="about_paragraph animate">
-            <h2 className="animate">WHO I AM?</h2>
-            <p className="my_paragraph animate">
-             
-              Hi, I am Kwaza Sakhumzi a Computer Science graduate with a software
-              development Learnership at Capaciti, I possess a versatile skill
-              set encompassing languages like HTML, CSS, JavaScript, ReactJs, 
-              Flutter, Dart, Arduino, C++, VB.NET, Java, and Python. My adaptability,
-              fostered through experiences like the MTN App Academy and hackathons, enables me
-              to navigate diverse programming environments effortlessly.
+      <div id="about">
+        <div className="container">
+          <div className="row">
+            <div className="about-col-1">
+              <img src="sakhumzi.png" alt="Profile" />
+            </div>
 
-              {/* Beyond coding, I find relaxation in watching football and
-              engaging in FIFA gaming during weekends. */}
-              Eager to contribute my technical proficiency and passion for innovation to challenging
-              software development endeavors, I am actively seeking 
-              opportunities to further enhance my skills and make a meaningful
-              impact in the field.
-
-              <p className="services">
-                <h5>WHAT I CAN OFFER?</h5>
-                <ul>
-                  <li>
-                    <div className="myservices">
-                      <h6>FRONTEND DEVELOPERMENT</h6>
-                      <p>Crafting intuitive and seamless user interfaces.</p>
-                      <p>Enhancing user experience and engagement.</p>
-                      <p>Utilizing clean, responsive, and accessible web designs.</p>
-
-                    </div>
-                  </li>
-                  <li>
-                    <div className="myservices">
-                      <h6>BACKEND DEVELOPERMENT</h6>
-                      <p>Building robust and scalable 
-                        server-side architectures
-                         and databases to power 
-                         dynamic and efficient
-                          web applications with
-                         secure and optimized functionality.</p>
-                    </div>
-                  </li>
-                  
-                </ul>
+            <div className="about-col-2">
+              <h2 className="sub-title">WHO I AM?</h2>
+              <p>
+              Hi, I am Kwaza Sakhumzi a Computer Science graduate with a software development Learnership at Capaciti,
+              I possess a versatile skill set encompassing languages like HTML, CSS, JavaScript, ReactJs, Flutter, Dart, 
+              Arduino, C++, VB.NET, Java, and Python. My adaptability, fostered through experiences like the MTN App Academy and hackathons, 
+              enables me to navigate diverse programming environments effortlessly.
               </p>
-              
-            </p>
+              <br />
+              <h4>WHAT I CAN DO?</h4>
+              <div className="tab-title">
+                
+                <p className={`tab-skill ${activeTab === 'education' ? 'active-skill' : ''}`} onClick={() => handleTabClick('education')}>GRAPHIC DESIGNER</p>
+                <p className={`tab-skill ${activeTab === 'skills' ? 'active-skill' : ''}`} onClick={() => handleTabClick('skills')}>FRONTEND DEVELOPER</p>
+                <p className={`tab-skill ${activeTab === 'experience' ? 'active-skill' : ''}`} onClick={() => handleTabClick('experience')}>BACKEND DEVELOPER</p>
+              </div>
+
+              <div className={`tab-container ${activeTab === 'education' ? 'active-container' : ''}`} id="education">
+                <ul>
+                  <li>Customized graphic design solutions tailored to your brand.</li>
+                  <li>Elevate your visual identity with my expert design services.</li>
+                  <li>Transform your ideas into stunning visuals with my graphic design expertise.</li>
+                </ul>
+              </div>
+              <div className={`tab-container ${activeTab === 'skills' ? 'active-container' : ''}`} id="skills">
+                <ul>
+                  <li>Crafting intuitive and seamless user interfaces.</li>
+                  <li>Enhancing user experience and engagement.</li>
+                  <li>Utilizing clean, responsive, and accessible web designs.</li>
+                </ul>
+              </div>
+              <div className={`tab-container ${activeTab === 'experience' ? 'active-container' : ''}`} id="experience">
+                <ul>
+                  <li>Developing robust server-side architectures and databases.</li>
+                  <li>Powering dynamic web apps with secure functionality.</li>
+                  <li>Creating scalable and optimized databases for efficient performance.</li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
       </div>
-  </section>
+    </section>
   );
 };
+
 export default Aboutpage;
